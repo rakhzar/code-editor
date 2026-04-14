9bc6aad756fc206d31f169eaaa3bd21f18a17439
rakhzar <mnv36ru@gmail.com>
2026-04-14 11:40:27 +0300

    chore(prettier): update config with single quotes and no semicolons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-15",
+  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ["./app/assets/css/main.css"],
+  css: ['./app/assets/css/main.css'],
   vite: {
     plugins: [tailwindcss()],
     server: {
       hmr: {
-        protocol: "ws",
-        host: "localhost",
+        protocol: 'ws',
+        host: 'localhost',
         port: 24678,
       },
       watch: { usePolling: true },
@@ -17,17 +17,17 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: "Code Editor",
+      title: 'Code Editor',
     },
   },
   googleFonts: {
-    display: "swap",
+    display: 'swap',
     families: {
-      "Open Sans": {
-        wght: "300..800",
-        ital: "300..800",
+      'Open Sans': {
+        wght: '300..800',
+        ital: '300..800',
       },
     },
   },
-  modules: ["@nuxtjs/google-fonts"],
-});
+  modules: ['@nuxtjs/google-fonts'],
+})
