fd32dfc06f9f28bffbccbba2e1fd0bbc13df0e33
rakhzar <mnv36ru@gmail.com>
2026-04-14 17:53:05 +0300

    refactor(AppHero): decompose into content and image components

---
+++ app/components/AppHeroImage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="lg:perspective-1000 relative scale-90 lg:w-2/3 lg:scale-110">
+    <div class="hero-reflection"></div>
+    <img src="/editor.png" alt="Editor UI" class="hero-image" />
+  </div>
+</template>
