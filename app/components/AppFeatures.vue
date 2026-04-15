5c5d73a6a5c54413ef4cc79f133ffeba6c55f4e9
rakhzar <mnv36ru@gmail.com>
2026-04-15 13:01:19 +0300

    refactor: rename AppFeaturesHeader to AppSectionHeader and add props

--- app/components/AppFeatures.vue
+++ app/components/AppFeatures.vue
@@ -3,7 +3,11 @@
     class="flex flex-col gap-y-8 bg-gray-50 py-8 sm:gap-y-12 sm:py-16 dark:bg-gray-700"
     id="features"
   >
-    <AppFeaturesHeader />
+    <AppSectionHeader
+      heading="Unlock the Full Potential"
+      subheading="With tailored settings, keyboard shortcuts, and powerful extensions, you
+      can work smarter and faster."
+    />
     <AppFeaturesContent />
   </section>
 </template>
