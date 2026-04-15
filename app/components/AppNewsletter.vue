01ace9ae05fd4af9a46c8c8e9313eeb81ce26dd0
rakhzar <mnv36ru@gmail.com>
2026-04-15 13:06:45 +0300

    refactor: remove duplicate AppNewsletterHeader, reuse AppSectionHeader

--- app/components/AppNewsletter.vue
+++ app/components/AppNewsletter.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="flex flex-col gap-y-6 py-8 sm:gap-y-8 sm:py-16">
-    <AppNewsletterHeader
+    <AppSectionHeader
       heading="Stay Updated"
       subheading="Subscribe to our newsletter for the latest updates and tips."
     />
