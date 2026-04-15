44bdded24313afe85228e4311c451b32c4a963b9
rakhzar <mnv36ru@gmail.com>
2026-04-15 10:07:16 +0300

    refactor(AppHeader): extracted NavItem component to reduce duplication

---
+++ app/components/AppHeaderNavItem.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    linkClasses?: string
+  }>(),
+  {
+    linkClasses:
+      'font-semibold hover:text-indigo-600 dark:text-gray-200 dark:hover:text-indigo-200',
+  },
+)
+</script>
+
+<template>
+  <li>
+    <a href="#" :class="linkClasses">
+      <slot />
+    </a>
+  </li>
+</template>
