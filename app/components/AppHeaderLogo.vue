819dd131d5efbfa963b733f4dfc4fd7ab918a8b8
rakhzar <mnv36ru@gmail.com>
2026-04-16 12:02:55 +0300

    refactor: replace <a href> with <NuxtLink> in logo

--- app/components/AppHeaderLogo.vue
+++ app/components/AppHeaderLogo.vue
@@ -1,11 +1,6 @@
 <template>
-  <a href="#" class="flex items-center gap-x-1 sm:gap-x-2">
-    <Icon
-      name="code"
-      class="size-8 -rotate-6 text-indigo-600 sm:size-12 dark:text-white"
-    />
-    <span class="text-xl font-black sm:text-2xl dark:text-white"
-      >Code Editor</span
-    >
-  </a>
+  <NuxtLink to="/" class="flex items-center gap-x-1 sm:gap-x-2">
+    <Icon name="code" class="size-8 -rotate-6 text-indigo-600 sm:size-12 dark:text-white" />
+    <span class="text-xl font-black sm:text-2xl dark:text-white">Code Editor</span>
+  </NuxtLink>
 </template>
