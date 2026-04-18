a3a40c2ce02270b63fecd14f5f5bfcead63bcbc9
rakhzar <mnv36ru@gmail.com>
2026-04-18 12:52:11 +0300

    feat(header): add /login link to Log In navigation item

--- app/components/AppHeaderNav.vue
+++ app/components/AppHeaderNav.vue
@@ -6,7 +6,7 @@
         link="/playlists"
         >Library</AppHeaderNavItem
       >
-      <AppHeaderNavItem>Log In</AppHeaderNavItem>
+      <AppHeaderNavItem link="/login">Log In</AppHeaderNavItem>
       <AppHeaderNavItem
         link="/register"
         link-classes="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-indigo-50 hover:bg-indigo-700"
