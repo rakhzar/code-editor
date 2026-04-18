4fd01aef4cb08002f5029e9933aa11c7c58139f1
rakhzar <mnv36ru@gmail.com>
2026-04-18 09:34:34 +0300

    refactor: replace hardcoded lessons with reusable components

---
+++ app/components/LessonListItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{ lesson: { number: number; title: string; duration: string } }>()
+
+const playlistSlug = useRoute().params.playlistSlug
+const lessonSlug = props.lesson.title.toLowerCase().replaceAll(' ', '-')
+</script>
+
+<template>
+  <li>
+    <NuxtLink
+      :to="`/playlists/${playlistSlug}/lessons/${lessonSlug}`"
+      class="flex w-full items-center justify-between gap-x-4 rounded-lg bg-indigo-50 p-3 text-indigo-700 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-700"
+    >
+      <span class="truncate text-sm font-medium sm:text-base">{{ lesson.number }}. {{ lesson.title }}</span>
+      <span class="text-sm text-gray-500 sm:text-base">{{ lesson.duration }}</span>
+    </NuxtLink>
+  </li>
+</template>
