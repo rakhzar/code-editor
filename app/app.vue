179b579cca40be417fc9727cd25022f51b2bea32
rakhzar <mnv36ru@gmail.com>
2026-04-18 11:06:00 +0300

    refactor(app.vue): remove unused component

--- app/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="dark flex min-h-screen flex-col dark:bg-gray-800">
-    <AppHeader />
-    <main>
-      <NuxtPage />
-    </main>
-    <AppFooter />
-  </div>
-</template>
