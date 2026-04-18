b0b79f43a97616fd3171786ebeeaf91dd4f98f50
rakhzar <mnv36ru@gmail.com>
2026-04-18 11:01:10 +0300

    feat(header): add /register route to Sign Up nav item

--- app/components/AppHeaderNav.vue
+++ app/components/AppHeaderNav.vue
@@ -1,9 +1,16 @@
 <template>
   <nav class="hidden grow sm:block">
     <ul class="flex items-center gap-x-8">
-      <AppHeaderNavItem class="mr-auto" link="/playlists">Library</AppHeaderNavItem>
+      <AppHeaderNavItem
+        class="mr-auto"
+        link="/playlists"
+        >Library</AppHeaderNavItem
+      >
       <AppHeaderNavItem>Log In</AppHeaderNavItem>
-      <AppHeaderNavItem link-classes="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-indigo-50 hover:bg-indigo-700">
+      <AppHeaderNavItem
+        link="/register"
+        link-classes="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-indigo-50 hover:bg-indigo-700"
+      >
         Sign Up
       </AppHeaderNavItem>
     </ul>
