8230715f665d7f29afa5fda98b2cb6a72d893d05
rakhzar <mnv36ru@gmail.com>
2026-04-18 13:21:31 +0300

    feat(mobile-nav): add toggle logic and login link

--- app/components/AppHeaderMobileNavButton.vue
+++ app/components/AppHeaderMobileNavButton.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-defineProps<{ icon: string }>()
+defineProps<{ icon: string }>();
 </script>
 
 <template>
-  <a class="-mr-1.5 rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
-    <Icon :name="icon" class="size-6 dark:text-white" />
+  <a class="-mr-1.5 cursor-pointer rounded-md p-2 hover:bg-gray-100 dark:hover:bg-gray-700">
+    <Icon
+      :name="icon"
+      class="size-6 dark:text-white"
+    />
   </a>
 </template>
--- app/components/AppHeaderMobileNavItem.vue
+++ app/components/AppHeaderMobileNavItem.vue
@@ -2,12 +2,15 @@
 const {
   link = '/',
   linkClasses = 'inline-block w-full rounded-md p-2 text-center text-sm font-semibold hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700',
-} = defineProps<{ link?: string; linkClasses?: string }>()
+} = defineProps<{ link?: string; linkClasses?: string }>();
 </script>
 
 <template>
   <li class="w-full">
-    <NuxtLink :to="link" :class="linkClasses">
+    <NuxtLink
+      :to="link"
+      :class="linkClasses"
+    >
       <slot />
     </NuxtLink>
   </li>
