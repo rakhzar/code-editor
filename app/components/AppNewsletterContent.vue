4f329f3068b5090d771507772426ec1c60e28204
rakhzar <mnv36ru@gmail.com>
2026-04-18 19:33:45 +0300

    refactor(newsletter): replace raw input with AppFormField

--- app/components/AppNewsletterContent.vue
+++ app/components/AppNewsletterContent.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="container">
     <form class="flex justify-center gap-x-2">
-      <input
-        type="email"
-        placeholder="Your Email"
-        class="w-96 rounded-md border border-gray-300 px-4 py-2 text-sm shadow-xs ring-transparent outline-hidden focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
-      />
+      <AppFormField type="email" placeholder="Your Email" id="email" class="w-96" />
       <button
         class="cursor-pointer rounded-md bg-indigo-600 px-4 py-1 font-semibold text-indigo-50 hover:bg-indigo-700 sm:rounded-lg sm:px-6 sm:py-2"
       >
