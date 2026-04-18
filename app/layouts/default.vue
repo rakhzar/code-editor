e819d6152e3df6843e4545e0e86b91d4c4744a6f
rakhzar <mnv36ru@gmail.com>
2026-04-18 11:06:58 +0300

    refactor(layouts): extract default page layout structure

---
+++ app/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="dark flex min-h-screen flex-col dark:bg-gray-800">
+    <AppHeader />
+    <main>
+      <NuxtPage />
+    </main>
+    <AppFooter />
+  </div>
+</template>
