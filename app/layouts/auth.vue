4252834a706fb23167387075bb9246808762c506
rakhzar <mnv36ru@gmail.com>
2026-04-18 12:43:50 +0300

    style(auth-layout): add background colors for light and dark modes

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="grid min-h-screen place-items-center">
+  <main class="dark grid min-h-screen place-items-center bg-gray-50 dark:bg-gray-800">
     <slot />
   </main>
 </template>
