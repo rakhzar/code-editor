112f62d86dce1c03a4ed663b200fba9f3f8db655
rakhzar <mnv36ru@gmail.com>
2026-04-19 10:37:11 +0300

    style: add responsive text sizes to AppButton

--- app/components/AppButton.vue
+++ app/components/AppButton.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="cursor-pointer rounded-md bg-indigo-600 px-4 py-1 font-semibold text-indigo-50 hover:bg-indigo-700 sm:rounded-lg sm:px-6 sm:py-2">
+  <button
+    class="cursor-pointer rounded-md bg-indigo-600 px-4 py-1 text-sm font-semibold text-indigo-50 hover:bg-indigo-700 sm:rounded-lg sm:px-6 sm:py-2 sm:text-base"
+  >
     <slot />
   </button>
 </template>
