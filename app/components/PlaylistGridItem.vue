d5d5850d26e5640f8dbe3a5cd11737ba26fd5990
rakhzar <mnv36ru@gmail.com>
2026-04-20 19:13:48 +0300

    fix: add leading slash to playlist route path"

--- app/components/PlaylistGridItem.vue
+++ app/components/PlaylistGridItem.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-const props = defineProps<{ playlist: { title: string; description: string; tags: string[] } }>()
+const props = defineProps<{
+  playlist: { title: string; description: string; tags: string[] };
+}>();
 
-const playlistSlug = props.playlist.title.toLowerCase().replaceAll(' ', '-')
+const playlistSlug = props.playlist.title
+  .toLowerCase()
+  .replaceAll(' ', '-');
 </script>
 
 <template>
-  <NuxtLink :to="`playlists/${playlistSlug}/lessons/getting-started-with-prettier`">
+  <NuxtLink
+    :to="`/playlists/${playlistSlug}/lessons/getting-started-with-prettier`"
+  >
     <article
       class="flex h-full flex-col rounded-md border border-gray-200 p-4 hover:border-indigo-600 sm:rounded-xl sm:p-6 dark:border-black dark:bg-gray-700 dark:hover:border-indigo-400"
     >
       <header>
-        <h3 class="font-semibold sm:text-lg dark:text-white">{{ playlist.title }}</h3>
+        <h3 class="font-semibold sm:text-lg dark:text-white">
+          {{ playlist.title }}
+        </h3>
       </header>
-      <p class="mt-2 mb-6 text-sm text-gray-600 sm:text-base dark:text-gray-400">
+      <p
+        class="mt-2 mb-6 text-sm text-gray-600 sm:text-base dark:text-gray-400"
+      >
         {{ playlist.description }}
       </p>
       <footer class="mt-auto">
