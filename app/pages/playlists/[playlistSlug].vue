73a45ad7cdecb83a034f70ddc175b3f83006dd31
rakhzar <mnv36ru@gmail.com>
2026-04-20 21:01:24 +0300

    feat: replace hardcoded playlist header with dynamic data from store

--- app/pages/playlists/[playlistSlug].vue
+++ app/pages/playlists/[playlistSlug].vue
@@ -1,15 +1,17 @@
+<script setup lang="ts">
+const playlist = usePlaylistsStore().getPlaylistBySlug(
+  useRoute().params.playlistSlug as string,
+);
+</script>
+
 <template>
-  <section
-    class="flex flex-col gap-y-8 pt-24 pb-12 sm:py-16 md:gap-y-12"
-  >
+  <section class="flex flex-col gap-y-8 pt-24 pb-12 sm:py-16 md:gap-y-12">
     <AppSectionHeader
       type="page"
-      heading="Prettier"
-      subheading="Automatically format your code to ensure consistent style across your project."
+      :heading="playlist?.title || 'Playlist not found'"
+      :subheading="playlist?.description || 'No description available'"
     />
-    <div
-      class="container grid grid-cols-1 gap-y-8 lg:grid-cols-3 lg:gap-x-8"
-    >
+    <div class="container grid grid-cols-1 gap-y-8 lg:grid-cols-3 lg:gap-x-8">
       <PlaylistLessons />
       <NuxtPage />
     </div>
