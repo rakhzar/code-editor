e5eee0f3721676030fd952a6eccee36311243063
rakhzar <mnv36ru@gmail.com>
2026-04-20 13:47:36 +0300

    refactor: add link prop to AppFeaturesCard and wrap content with NuxtLink for navigation

--- app/components/AppFeaturesCard.vue
+++ app/components/AppFeaturesCard.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-defineProps<{ feature: { icon: string; title: string; description: string } }>()
+defineProps<{
+  feature: {
+    icon: string;
+    title: string;
+    description: string;
+    link: string;
+  };
+}>();
 </script>
 
 <template>
-  <a href="#">
+  <NuxtLink :to="feature.link">
     <article
       class="h-full rounded-md border border-gray-200 bg-white p-4 hover:border-indigo-600 sm:rounded-xl sm:p-6 dark:border-black dark:bg-gray-800 dark:hover:border-indigo-400"
     >
@@ -12,13 +19,17 @@ defineProps<{ feature: { icon: string; title: string; description: string } }>()
           :name="feature.icon"
           class="size-10 text-indigo-600 sm:size-12 dark:text-indigo-400"
         />
-        <h3 class="font-semibold sm:text-lg dark:text-white">
+        <h3
+          class="font-semibold sm:text-lg dark:text-white"
+        >
           {{ feature.title }}
         </h3>
       </header>
-      <p class="mt-1 text-sm text-gray-600 sm:text-base dark:text-gray-400">
+      <p
+        class="mt-1 text-sm text-gray-600 sm:text-base dark:text-gray-400"
+      >
         {{ feature.description }}
       </p>
     </article>
-  </a>
+  </NuxtLink>
 </template>
--- app/components/AppFeaturesContent.vue
+++ app/components/AppFeaturesContent.vue
@@ -5,20 +5,23 @@ const features = [
     title: 'Settings',
     description:
       'Personalize your workspace to match your unique coding style and preferences effortlessly.',
+    link: '/playlists/prettier/lessons/getting-started-with-prettier',
   },
   {
     icon: 'fire',
     title: 'Keyboard Shortcuts',
     description:
       'Streamline your workflow with essential shortcuts that save you time and boost efficiency.',
+    link: '/playlists/prettier/lessons/getting-started-with-prettier',
   },
   {
     icon: 'puzzle',
     title: 'Extensions',
     description:
       'Expand your capabilities with powerful extensions tailored to enhance your coding experience.',
+    link: '/playlists/prettier/lessons/getting-started-with-prettier',
   },
-]
+];
 </script>
 
 <template>
