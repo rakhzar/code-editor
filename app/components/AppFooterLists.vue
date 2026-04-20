a585bf81fb71551de3c1ff9b25c53806b50ac12f
rakhzar <mnv36ru@gmail.com>
2026-04-20 14:03:43 +0300

    refactor: add nested items with links to footer lists and improve responsive layout

--- app/components/AppFooterLists.vue
+++ app/components/AppFooterLists.vue
@@ -2,21 +2,66 @@
 const lists = [
   {
     heading: 'Popular Playlists',
-    items: ['Prettier', 'GitLens', 'Live Share'],
+    items: [
+      {
+        label: 'Prettier',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'GitLens',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'Live Share',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+    ],
   },
   {
     heading: 'Сonfigurations',
-    items: ['Settings', 'Keyboard Shortcuts', 'Code Snippets'],
+    items: [
+      {
+        label: 'Settings',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'Keyboard Shortcuts',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'Code Snippets',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+    ],
   },
   {
     heading: 'Social',
-    items: ['YouTube', 'Telegram', 'GitHub'],
+    items: [
+      {
+        label: 'YouTube',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'Telegram',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+      {
+        label: 'GitHub',
+        link: '/playlists/prettier/lessons/getting-started-with-prettier',
+      },
+    ],
   },
-]
+];
 </script>
 
 <template>
-  <div class="flex flex-col gap-y-8 sm:flex-row sm:gap-x-16">
-    <AppFooterList v-for="list in lists" :key="list.heading" :list="list" />
+  <div
+    class="flex flex-col gap-y-8 sm:flex-row sm:gap-x-16"
+  >
+    <AppFooterList
+      v-for="list in lists"
+      :key="list.heading"
+      :list="list"
+    />
   </div>
 </template>
