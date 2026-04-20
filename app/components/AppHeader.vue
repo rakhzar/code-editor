16af425c085c68d565ca0a7ce06a61607302cc8d
rakhzar <mnv36ru@gmail.com>
2026-04-20 12:42:48 +0300

    refactor: replace AppLogo with static AppHeaderLogo and remove dynamic props

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -2,8 +2,12 @@
   <header
     class="fixed inset-x-0 top-0 z-10 border-b border-gray-200 bg-white py-1.5 sm:static sm:border-0 sm:py-4 dark:border-0 dark:bg-gray-900"
   >
-    <div class="container flex items-center justify-between gap-x-10">
-      <AppHeaderLogo />
+    <div
+      class="container flex items-center justify-between gap-x-10"
+    >
+      <AppLogo
+        icon-classes="size-8 text-indigo-600 sm:size-12 dark:text-white"
+      ></AppLogo>
       <AppHeaderNav />
       <AppHeaderMobileNav />
     </div>
---
+++ app/components/AppLogo.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  iconClasses: string;
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    to="/"
+    class="flex items-center gap-x-1 sm:gap-x-2"
+  >
+    <Icon
+      name="code"
+      :class="`-rotate-6 ${iconClasses}`"
+    />
+    <span
+      class="text-xl font-black sm:text-2xl dark:text-white"
+      >Code Editor</span
+    >
+  </NuxtLink>
+</template>
