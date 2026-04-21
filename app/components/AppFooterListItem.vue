57f2e4ba0318a30ea713c93708be1c00ce4b3c88
rakhzar <mnv36ru@gmail.com>
2026-04-21 10:48:03 +0300

    feat: add target="_blank" for external links in footer

--- app/components/AppFooterListItem.vue
+++ app/components/AppFooterListItem.vue
@@ -8,6 +8,7 @@ defineProps<{
   <li>
     <NuxtLink
       :to="link"
+      :target="link.startsWith('http') ? '_blank' : undefined"
       class="text-sm text-indigo-200 hover:text-white sm:text-base dark:text-gray-400"
     >
       <slot />
