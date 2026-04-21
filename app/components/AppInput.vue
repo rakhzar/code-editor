0edc20529d1f6230c5e4cfbab987b5d07e28dccf
rakhzar <mnv36ru@gmail.com>
2026-04-21 19:51:54 +0300

    feat(components): add base AppInput with defineModel support

--- app/components/AppInput.vue
+++ app/components/AppInput.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+</script>
+
 <template>
   <input
+    v-model="model"
     class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
   />
 </template>
