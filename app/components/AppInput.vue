ba7dc3d1478f334dd2de0609894c6d522634687c
rakhzar <mnv36ru@gmail.com>
2026-04-21 20:42:35 +0300

    feat(AppInput): add trim modifier to v-model

--- app/components/AppInput.vue
+++ app/components/AppInput.vue
@@ -4,7 +4,7 @@ const model = defineModel<string>();
 
 <template>
   <input
-    v-model="model"
+    v-model.trim="model"
     class="rounded-md border border-gray-300 px-4 py-2 text-sm shadow-sm focus:border-indigo-600 focus:ring-1 sm:rounded-lg sm:text-base dark:border-0 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-400 dark:focus:ring-indigo-400"
   />
 </template>
