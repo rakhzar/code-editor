864895e30426c7242daae63c23006ca8cf29a262
rakhzar <mnv36ru@gmail.com>
2026-04-21 20:33:25 +0300

    style(playlists): add items-start to prevent card stretching

--- app/pages/playlists/index.vue
+++ app/pages/playlists/index.vue
@@ -1,14 +1,12 @@
 <template>
-  <section
-    class="flex flex-col gap-y-8 pt-24 pb-12 sm:py-16 md:gap-y-12"
-  >
+  <section class="flex flex-col gap-y-8 pt-24 pb-12 sm:py-16 md:gap-y-12">
     <AppSectionHeader
       type="page"
       heading="Playlists"
       subheading="Learn how to make VSCode work for you with our comprehensive guides and tutorials."
     />
     <div
-      class="container grid grid-cols-1 gap-y-4 sm:gap-8 md:grid-cols-3 md:gap-x-6 xl:grid-cols-4 xl:gap-x-8"
+      class="container grid grid-cols-1 items-start gap-y-4 sm:gap-8 md:grid-cols-3 md:gap-x-6 xl:grid-cols-4 xl:gap-x-8"
     >
       <PlaylistFilters />
       <PlaylistGrid />
