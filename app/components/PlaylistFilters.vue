409747c6d076d394ef7d2ee8fb806b6678e14a0c
rakhzar <mnv36ru@gmail.com>
2026-04-21 19:51:22 +0300

    refactor(PlaylistFilters): replace native input with AppFormField component

--- app/components/PlaylistFilters.vue
+++ app/components/PlaylistFilters.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+const model = defineModel<string>();
+</script>
+
 <template>
   <div class="flex flex-col gap-y-4 md:col-span-1 lg:gap-y-6">
-    <AppFormField type="search" placeholder="Playlist Title" id="search" />
+    <AppFormField
+      v-model="model"
+      type="search"
+      placeholder="Playlist Title"
+      id="search"
+    />
     <div class="flex flex-col gap-y-2">
       <h3 class="font-semibold sm:text-lg dark:text-white">Tags</h3>
       <TagList />
