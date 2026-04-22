abdac10924c24610b737ed4e499c16f353e0debe
rakhzar <mnv36ru@gmail.com>
2026-04-22 09:41:02 +0300

    feat(TagList): connect click event to store.toggleTag

--- app/components/TagList.vue
+++ app/components/TagList.vue
@@ -8,6 +8,7 @@ const tagsStore = useTagsStore();
       v-for="tag in tagsStore.tags"
       :key="tag.id"
       :selected="tagsStore.selectedTagIds.includes(tag.id)"
+      @click="tagsStore.toggleTag(tag.id)"
     >
       {{ tag.name }}
     </TagListItem>
