33d9aa69b4bfbb19140d70b21e5407aa4249ef35
rakhzar <mnv36ru@gmail.com>
2026-04-22 09:37:37 +0300

    feat(TagListItem): add conditional classes for selected state and click handler

--- app/components/TagListItem.vue
+++ app/components/TagListItem.vue
@@ -1,7 +1,18 @@
+<script setup lang="ts">
+defineProps<{ selected: boolean }>();
+defineEmits(['click']);
+</script>
+
 <template>
   <li>
     <button
-      class="w-full rounded-md px-3 py-2 text-left text-sm text-gray-600 hover:bg-gray-100 sm:rounded-lg dark:text-gray-300 dark:hover:bg-gray-700"
+      @click="$emit('click')"
+      :class="[
+        'w-full cursor-pointer rounded-md px-3 py-2 text-left text-sm transition-colors sm:rounded-lg',
+        selected
+          ? 'bg-indigo-600 text-white hover:bg-indigo-700'
+          : 'text-gray-300 hover:bg-gray-700',
+      ]"
     >
       <slot />
     </button>
