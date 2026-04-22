638169d224b7b001ba4cd1a5a3543524f32402af
rakhzar <mnv36ru@gmail.com>
2026-04-22 13:53:00 +0300

    feat(PlaylistLessons): pass lessonIds prop to LessonList component

--- app/components/PlaylistLessons.vue
+++ app/components/PlaylistLessons.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+defineProps<{ lessonIds: number[] }>();
+</script>
+
 <template>
   <div class="flex flex-col gap-y-4 lg:col-span-1">
     <PlaylistLessonsHeader />
-    <LessonList />
+    <LessonList :lesson-ids="lessonIds" />
   </div>
 </template>
