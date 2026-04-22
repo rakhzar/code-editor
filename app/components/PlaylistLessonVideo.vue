5fc4936e1ae1f94efa79ecdb10e7df6b19cd58a6
rakhzar <mnv36ru@gmail.com>
2026-04-22 18:32:58 +0300

    feat(lesson-page): add 404 handling for playlist and lesson by slug

--- app/components/PlaylistLessonVideo.vue
+++ app/components/PlaylistLessonVideo.vue
@@ -1,10 +1,22 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
 <template>
   <iframe
     class="aspect-video w-full rounded-md sm:rounded-lg"
     src="https://www.youtube.com/embed/Ra563HfcxXU?list=PLXDouhCU5r6qRE46qQ2rYIPnbJ5a9jzmd"
-    title="Getting Started with Prettier"
+    :title="title"
     frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allow="
+      accelerometer;
+      autoplay;
+      clipboard-write;
+      encrypted-media;
+      gyroscope;
+      picture-in-picture;
+      web-share;
+    "
     referrerpolicy="strict-origin-when-cross-origin"
     allowfullscreen
   >
--- app/pages/playlists/[playlistSlug]/lessons/[lessonSlug].vue
+++ app/pages/playlists/[playlistSlug]/lessons/[lessonSlug].vue
@@ -25,7 +25,7 @@ if (!lesson || !playlist.lessonIds.includes(lesson.id)) {
 
 <template>
   <div class="flex flex-col gap-y-6 md:gap-y-8 lg:col-span-2">
-    <PlaylistLessonVideo />
+    <PlaylistLessonVideo :title="lesson.title" />
     <PlaylistLessonContent />
   </div>
 </template>
