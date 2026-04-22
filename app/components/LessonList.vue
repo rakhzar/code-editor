76e6441191a0ca7ce7bb0a5a003f58014b47c297
rakhzar <mnv36ru@gmail.com>
2026-04-22 14:12:34 +0300

    refactor(LessonList): replace hardcoded lessons with store-based computed data

--- app/components/LessonList.vue
+++ app/components/LessonList.vue
@@ -1,25 +1,24 @@
 <script setup lang="ts">
-const lessons = [
-  {
-    number: 1,
-    title: 'Getting Started with Prettier',
-    duration: '5:30',
-  },
-  {
-    number: 2,
-    title: 'Advanced GitLens Features',
-    duration: '8:45',
-  },
-  {
-    number: 3,
-    title: 'Debugging with ESLint',
-    duration: '6:15',
-  },
-]
+const props = defineProps<{ lessonIds: number[] }>();
+
+const lessonsStore = useLessonsStore();
+
+const lessons = computed(() => {
+  return lessonsStore.getLessonsByIds(props.lessonIds).map((lesson, index) => {
+    return {
+      ...lesson,
+      number: index + 1,
+    };
+  });
+});
 </script>
 
 <template>
   <ul class="flex flex-col gap-y-1">
-    <LessonListItem v-for="lesson in lessons" :key="lesson.number" :lesson="lesson" />
+    <LessonListItem
+      v-for="lesson in lessons"
+      :key="lesson.id"
+      :lesson="lesson"
+    />
   </ul>
 </template>
