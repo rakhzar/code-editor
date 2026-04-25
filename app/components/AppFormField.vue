a93991f246e0170f61be8ae52c7b5dc69e4a661b
rakhzar <mnv36ru@gmail.com>
2026-04-25 11:12:36 +0300

    feat: add AppFormField component with label and error handling

--- app/components/AppFormField.vue
+++ app/components/AppFormField.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 defineOptions({ inheritAttrs: false });
-defineProps<{ id: string }>();
+defineProps<{ id: string; error?: string }>();
 </script>
 
 <template>
   <div class="flex flex-col gap-y-1">
-    <AppLabel v-if="$slots.default" :for="id">
+    <AppLabel
+      v-if="$slots.default"
+      :for="id"
+    >
       <slot />
     </AppLabel>
-    <AppInput :id="id" v-bind="$attrs" />
+    <AppInput
+      :id="id"
+      v-bind="$attrs"
+    />
+    <p
+      v-if="error"
+      class="text-sm text-red-600 dark:text-red-400"
+    >
+      {{ error }}
+    </p>
   </div>
 </template>
