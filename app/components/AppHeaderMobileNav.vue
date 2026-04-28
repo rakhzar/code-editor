2bb4812f5123e0a846d162109df9e78d6285758d
rakhzar <mnv36ru@gmail.com>
2026-04-28 19:34:16 +0300

    refactor: update mobile navigation structure and conditional auth links

--- app/components/AppHeaderMobileNav.vue
+++ app/components/AppHeaderMobileNav.vue
@@ -1,19 +1,48 @@
+<script lang="ts" setup>
+const { loggedIn } = useUserSession();
+</script>
+
 <template>
   <div class="flex sm:hidden">
-    <nav id="nav" class="peer absolute inset-x-0 top-full hidden border-b border-gray-200 bg-white target:block dark:border-0 dark:bg-gray-900">
+    <nav
+      id="nav"
+      class="peer absolute inset-x-0 top-full hidden border-b border-gray-200 bg-white target:block dark:border-0 dark:bg-gray-900"
+    >
       <ul class="flex flex-col gap-y-2 px-3 pb-3">
         <li>
-          <AppHeaderMobileNavLink to="/playlists">Library</AppHeaderMobileNavLink>
+          <AppHeaderMobileNavLink to="/playlists"
+            >Library</AppHeaderMobileNavLink
+          >
         </li>
-        <li>
-          <AppHeaderMobileNavLink to="/login">Log In</AppHeaderMobileNavLink>
-        </li>
-        <li>
-          <AppButton to="/register" class="inline-block w-full text-center" lg>Sign Up</AppButton>
-        </li>
+        <template v-if="loggedIn">
+          <li>
+            <AppHeaderMobileNavLink to="#">Logout</AppHeaderMobileNavLink>
+          </li>
+        </template>
+        <template v-else>
+          <li>
+            <AppHeaderMobileNavLink to="/login">Log In</AppHeaderMobileNavLink>
+          </li>
+          <li>
+            <AppButton
+              to="/register"
+              class="inline-block w-full text-center"
+              lg
+              >Sign Up</AppButton
+            >
+          </li>
+        </template>
       </ul>
     </nav>
-    <AppHeaderMobileNavButton href="#nav" icon="bars" class="peer-target:hidden peer-target:[&+a]:block" />
-    <AppHeaderMobileNavButton href="#" icon="xMark" class="hidden" />
+    <AppHeaderMobileNavButton
+      href="#nav"
+      icon="bars"
+      class="peer-target:hidden peer-target:[&+a]:block"
+    />
+    <AppHeaderMobileNavButton
+      href="#"
+      icon="xMark"
+      class="hidden"
+    />
   </div>
 </template>
