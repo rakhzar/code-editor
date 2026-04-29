8d1907a5e81f1f18ba758de06a9676ed58e9ede0
rakhzar <mnv36ru@gmail.com>
2026-04-29 18:32:20 +0300

    refactor: replace manual auth check with AuthState component in desktop nav

--- app/components/AppHeaderNav.vue
+++ app/components/AppHeaderNav.vue
@@ -1,26 +1,22 @@
-<script lang="ts" setup>
-const { loggedIn } = useUserSession();
-</script>
-
 <template>
   <nav class="hidden grow sm:block">
     <ul class="flex items-center gap-x-8">
       <li class="mr-auto">
         <AppHeaderNavLink to="/playlists">Library</AppHeaderNavLink>
       </li>
-      <template v-if="loggedIn">
-        <li>
+      <AuthState v-slot="{ loggedIn, clear }">
+        <li v-if="loggedIn">
           <AppHeaderNavLink to="#">Logout</AppHeaderNavLink>
         </li>
-      </template>
-      <template v-else>
-        <li>
-          <AppHeaderNavLink to="/login">Log In</AppHeaderNavLink>
-        </li>
-        <li>
-          <AppButton to="/register">Sign Up</AppButton>
-        </li>
-      </template>
+        <template v-else>
+          <li>
+            <AppHeaderNavLink to="/login">Log In</AppHeaderNavLink>
+          </li>
+          <li>
+            <AppButton to="/register">Sign Up</AppButton>
+          </li>
+        </template>
+      </AuthState>
     </ul>
   </nav>
 </template>
